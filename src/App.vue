8a94b10469929ea1acd7d9a8c75221dad8aced2d
Johnson Chen <chnejohnson@gmail.com>
2024-05-03 13:38:23 +0800

    copy @vue-dapp/modal and init nuxt-app

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { BrowserWalletConnector, useVueDapp } from '@vue-dapp/core'
+import VueDappModal from './VueDappModal.vue'
+import { useVueDappModal } from './store'
+
+const store = useVueDappModal()
+
+const isModalOpen = ref(false)
+
+const { error, isConnected, address, chainId, disconnect, addConnector } = useVueDapp()
+
+addConnector(new BrowserWalletConnector())
+
+function onClickConnectButton() {
+	if (!isConnected.value) {
+		isModalOpen.value = true
+		// store.open()
+	} else {
+		disconnect()
+	}
+}
+
+function connectErrorHandler(err: any) {
+	console.error('ConnectError:', err.message)
+}
+function autoConnectErrorHandler(err: any) {
+	console.error('AutoConnectError:', err.message)
+}
+</script>
+
+<template>
+	<div>
+		<button @click="onClickConnectButton">{{ isConnected ? 'Disconnect' : 'Connect Wallet' }}</button>
+		<button @click="store.open">open</button>
+
+		<div>{{ error }}</div>
+		<p v-if="chainId">Chain ID: {{ chainId }}</p>
+		<p>{{ address }}</p>
+
+		<VueDappModal
+			dark
+			v-model="isModalOpen"
+			autoConnect
+			autoConnectBrowserWalletIfSolo
+			@connectError="connectErrorHandler"
+			@autoConnectError="autoConnectErrorHandler"
+		/>
+	</div>
+</template>
---
+++ vite.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+	plugins: [
+		vue(),
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+	build: {
+		assetsDir: 'assets',
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: '@vue-dapp/modal',
+			fileName: 'modal',
+		},
+		outDir: 'dist',
+		rollupOptions: {
+			external: ['vue', 'pinia', '@vue-dapp/core'],
+			output: {
+				dir: 'dist',
+				globals: {
+					vue: 'vue',
+					pinia: 'pinia',
+					'@vue-dapp/core': 'core',
+				},
+			},
+		},
+	},
+})
